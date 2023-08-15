3b1f7f0006980a108e17a81b4607571a54ae190c
Laszlo Sarvold <laszlo@live.com.ar>
2023-08-15 14:32:02 -0300

    minimal implementation of react-native-inappbrowser-reborn that reproduces TypeError: Cannot read property 'isAvailable' of null

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import InAppBrowser from 'react-native-inappbrowser-reborn';
 
 export default function App() {
+
+  const promptAuthorizationCode = async () => {
+    let code = '';
+    // In-app browsing:
+    try {
+      console.log('InAppBrowser is actually an object! ', InAppBrowser);
+      if (await InAppBrowser.isAvailable()) {
+        console.log('Never reaches this code due to [TypeError: Cannot read property \'isAvailable\' of null]');
+        const result = await InAppBrowser.openAuth(
+          'www.example.com',
+          `https://some.auth-api.com/void/callback`
+        );
+      }
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Authentication failed!', error);
+    } finally {
+      return code;
+    }
+  }
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Open inapp browser" onPress={promptAuthorizationCode} />
     </View>
   );
 }
